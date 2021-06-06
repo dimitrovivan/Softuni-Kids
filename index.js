8e2181e8d32a7f198b34a6ec8d7743353059a326
Ivan Dimitrov <bgivandimitrov@gmail.com>
2021-06-06 09:57:41 +0300

    Fixed dropdown issue when clicking or burger menu

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const burgerMenu = document.querySelector('.burger-menu-wrapper');
+const body = document.querySelector('body');
 const navigationList = document.querySelector('.top-navigation__list');
 
 burgerMenu.addEventListener('click', toggleBurgerMenu);
@@ -7,10 +8,12 @@ function toggleBurgerMenu() {
     if(burgerMenu.classList.contains('open')) {
         burgerMenu.classList.remove('open');
         navigationList.classList.remove('appear');
+        body.style.overflowY = "scroll";
 
     } else {
         burgerMenu.classList.add('open');
         navigationList.classList.add('appear');
+        body.style.overflowY = "hidden";
     }
 
 }
